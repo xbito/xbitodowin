8cfe4ad78a411765e53015f170d19bf2b6077bf9
Fernando Gutierrez <fernando.gutierrez@gmail.com>
2024-09-02 16:09:12 -0400

    Update to dark mode

--- stylesheet.py
+++ stylesheet.py
@@ -1,24 +1,27 @@
 UI_STYLESHEET = """
 QMainWindow {
-    background-color: #F5F5F5;
+    background-color: #2E2E2E;
+    color: #FFFFFF;
 }
 QGroupBox {
-    border: 1px solid #CCCCCC;
+    border: 1px solid #444444;
     border-radius: 5px;
     margin-top: 0.5em;
-    background-color: #FFFFFF;
+    background-color: #3E3E3E;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
 }
 QGroupBox::title {
     subcontrol-origin: margin;
     left: 10px;
     padding: 0 3px 0 3px;
+    color: #FFFFFF;
 }
 QLabel {
-    color: #333333;
+    color: #FFFFFF;
 }
 QRadioButton {
     padding: 3px;
+    color: #FFFFFF;
 }
 QRadioButton::indicator {
     width: 13px;
@@ -33,26 +36,43 @@ QRadioButton::indicator::checked {
     background-color: #1E90FF;
 }
 QLineEdit {
-    border: 1px solid #CCCCCC;
+    border: 1px solid #444444;
     border-radius: 5px;
     padding: 5px;
-    background-color: #FFFFFF;
+    background-color: #3E3E3E;
+    color: #FFFFFF;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
 }
 QTableWidget {
-    border: 1px solid #CCCCCC;
+    border: 1px solid #444444;
     border-radius: 5px;
-    background-color: #FFFFFF;
+    background-color: #3E3E3E;
+    color: #FFFFFF;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
 }
 QTableWidget QHeaderView::section {
-    background-color: #F5F5F5;
+    background-color: #2E2E2E;
     padding: 5px;
-    border: 1px solid #E0E0E0;
+    border: 1px solid #444444;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+    color: #FFFFFF;
 }
 QTableWidget QTableCornerButton::section {
-    background-color: #F5F5F5;
-    border: 1px solid #E0E0E0;
+    background-color: #2E2E2E;
+    border: 1px solid #444444;
+}
+TaskListSidebar {
+    border: 1px solid #444444;
+    border-radius: 5px;
+    background-color: #3E3E3E;
+    color: #FFFFFF;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+}
+TaskListSidebar::item {
+    padding: 5px;
+}
+TaskListSidebar::item:selected {
+    background-color: #1E90FF;
+    color: #FFFFFF;
 }
 """
